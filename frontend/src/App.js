3555350ef91a637af7a5b6d8ae512cc024b60091
Zerihun M <zersena@gmail.com>
2022-04-06 10:47:45 +0300

    first configuration

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">Ecommerce</a>
+      </header>
+      <main>list of products</main>
+    </div>
+  );
+}
+
+export default App;
